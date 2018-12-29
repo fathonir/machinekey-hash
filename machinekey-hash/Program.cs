f402c5db1df68ff4676efc849c4420e213384b65
Fathoni <m.fathoni@mail.com>
2018-12-30 00:28:59 +0700

    Add project files.

---
+++ machinekey-hash/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace machinekey_hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
